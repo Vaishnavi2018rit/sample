5ac522ea3e54bad75ff623a0bd31ed7a30355956
Vaishnavi <953618104047@ritrjpm.ac.in>
2020-12-09 12:34:02 +0530

    commit1

--- src/arraydupli/Arraydupli.java
+++ src/arraydupli/Arraydupli.java
@@ -5,6 +5,8 @@
  */
 package arraydupli;
 
+import java.util.*;
+
 /**
  *
  * @author vaishu
@@ -16,7 +18,43 @@ public class Arraydupli {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world");
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n=s.nextInt();
+        int i,j,t,v=0;
+        int[] a=new int[n];
+        int[] b= new int[n];
+        System.out.println("Enter the elements");
+        for(i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+            }
+        }
+        for( i=0;i<n;i++)
+        {
+          if(a[i]!=v && a[i]!=0)
+             b[i]=a[i];
+          v=a[i];
+        }
+        System.out.println("Array elements after removing duplicates are:");
+        for( i=0;i<b.length;i++)
+        {
+            if(b[i]!=0)
+             System.out.println(b[i]+"\t");
+        }
+        
+        
     }
     
 }
